dfcf92f9463d1eddfb967373dc98510d80609ac7
snizhok <snizhok@users.noreply.github.com>
2018-01-11 02:11:41 +0200

    Reverse Polish notation parser

--- Parser.php
+++ Parser.php
@@ -1,15 +1,10 @@
 <?php
-
 class Parser
 {
     const PATTERN = '/^([\+\-\*\/\(\)]|\d+)/';
-
     protected $output = [];
-
     protected $stack = [];
-
     protected $evaluationStack = [];
-
     protected $stackPriority = [
         '(' => 0,
         ')' => 0,
@@ -18,16 +13,15 @@ class Parser
         '*' => 2,
         '/' => 2,
     ];
+    protected $prevValue;
 
     public function evaluate($input)
     {
         $input = preg_replace('/\s+/', '', $input);
-
         while (strlen($input)) {
             preg_match(self::PATTERN, $input, $match);
             $input = substr($input, strlen($match[1]));
             $value = $match[1];
-
             if (!is_numeric($this->prevValue) && !is_numeric($value)
                 && $value != '(' && $this->prevValue != ')') {
                 if (in_array($value, ['*', '/'])) {
@@ -37,7 +31,6 @@ class Parser
                 }
             }
             $this->prevValue = $value;
-
             if (is_numeric($value)) {
                 $this->output[] = $value;
                 continue;
@@ -49,15 +42,19 @@ class Parser
                 case '*':
                 case '/':
                     $stackVal = end($this->stack);
-                    $stackValPriority = $this->stackPriority[$stackVal];
-                    while ($valPriority <= $stackValPriority) {
-                        if ($stackVal == '(') {
-                            break;
-                        }
-                        $stackVal = array_pop($this->stack);
-                        $this->output[] = $stackVal;
-                        $stackVal = end($this->stack);
+                    if (isset($this->stackPriority[$stackVal])) {
                         $stackValPriority = $this->stackPriority[$stackVal];
+                        while ($valPriority <= $stackValPriority) {
+                            if ($stackVal == '(') {
+                                break;
+                            }
+                            $stackVal = array_pop($this->stack);
+                            $this->output[] = $stackVal;
+                            $stackVal = end($this->stack);
+                            $stackValPriority = isset($this->stackPriority[$stackVal])
+                                ? $this->stackPriority[$stackVal]
+                                : 0;
+                        }
                     }
                     $this->stack[] = $value;
                     break;
@@ -77,7 +74,6 @@ class Parser
             $stackVal = array_pop($this->stack);
             $this->output[] = $stackVal;
         }
-
         foreach ($this->output as $value) {
             if (is_numeric($value)) {
                 $this->evaluationStack[] = $value;
@@ -105,7 +101,6 @@ class Parser
         return array_shift($this->evaluationStack);
     }
 }
-
 $parser = new Parser();
 // Evaluate an expression
-$value = $parser->evaluate('200+12*((1/8)+1)-19');
+var_dump($parser->evaluate('200+12*((1/8)+1)-19'));
