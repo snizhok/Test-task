601c06e6e3c32ba75b5b3ad5ad252a1decdf6c48
snizhok <snizhok@users.noreply.github.com>
2018-01-11 02:02:17 +0200

    Update BNFParser.php

--- BNFParser.php
+++ BNFParser.php
@@ -20,8 +20,7 @@ class BNFParser
     protected $digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     protected $mults = ['*', '/'];
     protected $adds = ['+', '-'];
-
-    protected $result;
+    
     protected $expression;
 
     public function evaluate($expression)
